f3975c43b27de97b648268fc4f5267d2bf72e06d
iwirsing <ivymolina@gmail.com>
2022-10-11 14:44:52 -0500

    commit with the first revision of password generator

---
+++ README.md
@@ -0,0 +1,28 @@
+# Module 3 Challenge: Password Generator
+This project allows the user to generate a password based on prompted criteria and also allows user to copy it to the clipboard for convenience.
+
+This project is emphasized on the use of JavaScript to create a randomly generated secure password for the user.
+
+## <u>Table of Contents</u>
+1. [Link to Deployed Page](#udeployed-pageu)
+2. [Webpage Features](#uwebpage-featuresu)
+3. [Screenshot](#uscreenshotu)
+4. [Sources](#usources-u)
+
+
+## <u>Deployed Page</u>
+
+## <u>Webpage Features</u>
+- The user can choose the password length from 8 to 128. If a number outside the range is chosen or a letter is typed in, it will later on alert an error and restart the process.
+- The webpage will generate a password according to the user's character type choices. When the user chooses none, it will alert an error and restart the process.
+- The user can copy the password into the clipboard for convenience. The webpage will then alert that the password has been copied.
+- The prompted criteria are stored in an object before being sent to a function that generates the actual password.
+
+
+## <u>Screenshot</u>
+
+![ScreenShot](./assets/passwordgenerator.png)
+
+## <u>Sources </u>
+1. https://www.w3schools.com/
+2. https://techozu.com/clipboard-api-domexception-javascript-fix/
---
+++ script.js
@@ -0,0 +1,106 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+var copyBtn = document.querySelector("#copy");
+var passwordLength = 0; //initialize
+var passwordIfUppercase;
+var passwordIfLowercase;
+var passwordIfNumeric;
+var passwordIfSpecialCharacters;
+
+//when generate button pressed, prompts start to find characters of password
+
+//Function 1
+// Prompt for criteria and write password to the #password input
+function writePassword() {
+
+  //prompt for password characters and store in an object
+  var passwordType={
+  passwordLength: window.prompt("From 8 to 128, how long will your password be?"),
+  passwordIfLowercase: window.confirm("Would you like lowercase characters in your password??"),
+  passwordIfUppercase: window.confirm("Would you like UPPERCASE characters in your password?"),
+  passwordIfNumeric: window.confirm("Include numeric characters?"),
+  passwordIfSpecialCharacters: window.confirm("Include special characters?")
+  }
+  
+  //check if first entry is valid
+  if ((passwordType.passwordLength<8)||(passwordType.passwordLength>128) || (isNaN(passwordType.passwordLength))){
+    window.alert("Password length error: Choose a number from 8 to 128. Kindly start over.");
+   //prompt again or restart
+    writePassword();
+  }
+  //check if there is any input for character type, if all false then
+   else if ((!passwordType.passwordIfUppercase)&&(!passwordType.passwordIfLowercase)&&(!passwordType.passwordIfNumeric)&&(!passwordType.passwordIfSpecialCharacters)){
+    window.alert("Password type error: Choose at least one type of characters. Kindly start over.");
+    //prompt again or restart
+    writePassword(); 
+  }
+  else{
+    //show selection choice and generate the password
+    window.alert("Password summary-- "+passwordType.passwordLength+" characters || uppercase: "+passwordType.passwordIfUppercase+"|| lowercase: "+passwordType.passwordIfLowercase+"|| numeric: "+passwordType.passwordIfNumeric+"|| special characters: "+passwordType.passwordIfSpecialCharacters);
+    var password = generatePassword(passwordType);
+    var passwordText=document.querySelector("#password");
+    //print password without commas
+    passwordText.value=password.join(''); 
+  }
+  
+  return;
+}
+ 
+//Function 2  
+//Generating Password with said criteria
+function generatePassword(passwordObject){
+  const uppercaseLetters="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  const lowercaseLetters="abcdefghijklmnopqrstuvwxyz";
+  const numeric="0123456789";
+  const specialCharacters=" !\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+  var passwordChoice=[]; 
+  
+  //build the passwordChoice string 
+  //if uppercase is confirmed
+  if (passwordObject.passwordIfUppercase===true){
+    passwordChoice=passwordChoice+uppercaseLetters;
+  }
+  //if lowercase is confirmed
+  if (passwordObject.passwordIfLowercase===true){
+    passwordChoice=passwordChoice+lowercaseLetters;
+  }
+  //if numeric is confirmed
+  if (passwordObject.passwordIfNumeric===true){
+    passwordChoice=passwordChoice+numeric;
+  }
+  //if special characters is confirmed
+  if (passwordObject.passwordIfSpecialCharacters===true){
+    passwordChoice=passwordChoice+specialCharacters;
+  }
+  
+  //stores the generated password
+  var generatedPassword=[];
+
+  //generate random characters from built up string
+  for (let i=0; i<passwordObject.passwordLength; i++){
+    //LetChar stores the random characters
+    var LetChar=passwordChoice[Math.floor(Math.random()*passwordChoice.length)];
+    generatedPassword[i]=LetChar;
+  }
+  //returns generated password
+  return generatedPassword;
+}
+
+//Function 3 
+// Copy the Password into the clipboard
+async function copyPassword(){
+  var copyPass=document.getElementById("password");
+  copyPass.select();
+  await parent.navigator.clipboard.writeText(copyPass.value);
+  // prompt the user of the copied password
+  window.alert("Password "+ copyPass.value+" is copied.")
+ 
+}
+
+
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+// //Add event listener to copy to clipboard button
+copyBtn.addEventListener("click", copyPassword);
