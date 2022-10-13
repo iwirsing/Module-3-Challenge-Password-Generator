a37c94fef981a75f73634b9f2f90dbaa75ceed1a
iwirsing <ivymolina@gmail.com>
2022-10-13 15:16:27 -0500

    changed js script, validate EVERY prompt selection of user

--- script.js
+++ script.js
@@ -7,40 +7,80 @@ var passwordIfLowercase;
 var passwordIfNumeric;
 var passwordIfSpecialCharacters;
 
-//Function 1
+// Function 1
 // Prompt for criteria and write password to the #password input
 function writePassword() {
-  //prompt for password characters and store input inside an object
+  //initiate object to store prompt answers for password criteria 
   var passwordType={
-  passwordLength: window.prompt("From 8 to 128, how long will your password be?"),
-  passwordIfLowercase: window.confirm("Would you like lowercase characters in your password??"),
-  passwordIfUppercase: window.confirm("Would you like UPPERCASE characters in your password?"),
-  passwordIfNumeric: window.confirm("Include numeric characters?"),
-  passwordIfSpecialCharacters: window.confirm("Include special characters?")
+  passwordLength: window.prompt("From 8 to 128, how long will your password be?")
   }
   
   //check if password length is valid: between 8 and 128
   if ((passwordType.passwordLength<8)||(passwordType.passwordLength>128) || (isNaN(passwordType.passwordLength))){
-    window.alert("Password length error: Choose a number from 8 to 128. Kindly start over.");
-    //prompt again or restart
-    writePassword();
-  }
-  //check if there is any input for character type: if selection for all types is false then, none was selected
-  else if ((!passwordType.passwordIfUppercase)&&(!passwordType.passwordIfLowercase)&&(!passwordType.passwordIfNumeric)&&(!passwordType.passwordIfSpecialCharacters)){
-    window.alert("Password type error: Choose at least one type of characters. Kindly start over.");
-    //prompt again or restart
-    writePassword(); 
-  }
-  // if all criteria are met, password will be generated and written
-  else{
-    //show selection choice summary and generate the password
-    window.alert("Password summary-- "+passwordType.passwordLength+" characters || uppercase: "+passwordType.passwordIfUppercase+"|| lowercase: "+passwordType.passwordIfLowercase+"|| numeric: "+passwordType.passwordIfNumeric+"|| special characters: "+passwordType.passwordIfSpecialCharacters);
-    var password = generatePassword(passwordType);
+    //print error in password screen
     var passwordText=document.querySelector("#password");
-    //print password without commas
-    passwordText.value=password.join(''); 
+    passwordText.value="Password length error: Choose a number from 8 to 128. Kindly start over.";
   }
+  else //confirm selection and continue prompting
+  {
+    alert("Your password will have "+passwordType.passwordLength+" characters.");
+
+    //LOWERCASE: prompt for lowercase and store it in the object
+    Object.assign(passwordType,{ passwordIfLowercase: window.confirm("Would you like lowercase characters in your password?")})
+    //confirm selection for lowercase
+    if (passwordType.passwordIfLowercase===true){
+      alert("Your password will have lowercase characters.");
+    }
+    else{
+      alert("Your password will have NO lowercase characters.");
+    }
+    
+    //UPPERCASE: prompt for UPPERCASE and store it in the object
+    Object.assign(passwordType,{ passwordIfUppercase: window.confirm("Would you like UPPERCASE characters in your password?")})
+    //confirm selection for UPPERCASE
+    if (passwordType.passwordIfUppercase===true){
+      alert("Your password will have UPPERCASE characters.");
+    }
+    else{
+      alert("Your password will have NO UPPERCASE characters.");
+    }
+
+    //NUMERIC: prompt for numeric and store it in the object
+    Object.assign(passwordType,{ passwordIfNumeric: window.confirm("Would you like numeric characters in your password?")})
+    //confirm selection for numeric
+    if (passwordType.passwordIfNumeric===true){
+      alert("Your password will have numeric characters.");
+    }
+    else{
+      alert("Your password will have NO numeric characters.");
+    }
+
+    //SPECIAL CHARACTERS: prompt for special characters and store it in the object
+    Object.assign(passwordType,{ passwordIfSpecialCharacters: window.confirm("Would you like special characters in your password?")})
+    //confirm selection for special characters
+    if (passwordType.passwordIfSpecialCharacters===true){
+      alert("Your password will have special characters.");
+    }
+    else{
+      alert("Your password will have NO special characters.");
+    }
   
+    //check at least one type of character selected: if selection for all types is false then, none was selected
+    if ((!passwordType.passwordIfUppercase)&&(!passwordType.passwordIfLowercase)&&(!passwordType.passwordIfNumeric)&&(!passwordType.passwordIfSpecialCharacters)){
+      //print error in password screen
+      var passwordText=document.querySelector("#password");
+      passwordText.value="Password generation error: Please choose at least one type of characters. Kindly start over.";
+    }
+    // if all criteria are met, password will be generated and written
+    else{
+      //show selection choice summary and generate the password
+      window.alert("Password summary-- "+passwordType.passwordLength+" characters, lowercase: "+passwordType.passwordIfLowercase+", UPPERCASE: "+passwordType.passwordIfUppercase+" , numeric: "+passwordType.passwordIfNumeric+", and special characters: "+passwordType.passwordIfSpecialCharacters);
+      var password = generatePassword(passwordType);
+      var passwordText=document.querySelector("#password");
+      //print password without commas
+      passwordText.value=password.join(''); 
+    }
+  }
   return;
 }
  
@@ -96,9 +136,8 @@ async function copyPassword(){
 }
 
 
-
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-// //Add event listener to copy to clipboard button
+//Add event listener to copy to clipboard button
 copyBtn.addEventListener("click", copyPassword);
