c146c1431d3982a2fb05a78704f134676f840795
iwirsing <ivymolina@gmail.com>
2022-10-13 14:27:12 -0500

    cleaned up JS file

--- script.js
+++ script.js
@@ -1,19 +1,16 @@
-// Assignment Code
+// Assignment Codes
 var generateBtn = document.querySelector("#generate");
 var copyBtn = document.querySelector("#copy");
-var passwordLength = 0; //initialize
+var passwordLength = 0; 
 var passwordIfUppercase;
 var passwordIfLowercase;
 var passwordIfNumeric;
 var passwordIfSpecialCharacters;
 
-//when generate button pressed, prompts start to find characters of password
-
 //Function 1
 // Prompt for criteria and write password to the #password input
 function writePassword() {
-
-  //prompt for password characters and store in an object
+  //prompt for password characters and store input inside an object
   var passwordType={
   passwordLength: window.prompt("From 8 to 128, how long will your password be?"),
   passwordIfLowercase: window.confirm("Would you like lowercase characters in your password??"),
@@ -22,20 +19,21 @@ function writePassword() {
   passwordIfSpecialCharacters: window.confirm("Include special characters?")
   }
   
-  //check if first entry is valid
+  //check if password length is valid: between 8 and 128
   if ((passwordType.passwordLength<8)||(passwordType.passwordLength>128) || (isNaN(passwordType.passwordLength))){
     window.alert("Password length error: Choose a number from 8 to 128. Kindly start over.");
-   //prompt again or restart
+    //prompt again or restart
     writePassword();
   }
-  //check if there is any input for character type, if all false then
-   else if ((!passwordType.passwordIfUppercase)&&(!passwordType.passwordIfLowercase)&&(!passwordType.passwordIfNumeric)&&(!passwordType.passwordIfSpecialCharacters)){
+  //check if there is any input for character type: if selection for all types is false then, none was selected
+  else if ((!passwordType.passwordIfUppercase)&&(!passwordType.passwordIfLowercase)&&(!passwordType.passwordIfNumeric)&&(!passwordType.passwordIfSpecialCharacters)){
     window.alert("Password type error: Choose at least one type of characters. Kindly start over.");
     //prompt again or restart
     writePassword(); 
   }
+  // if all criteria are met, password will be generated and written
   else{
-    //show selection choice and generate the password
+    //show selection choice summary and generate the password
     window.alert("Password summary-- "+passwordType.passwordLength+" characters || uppercase: "+passwordType.passwordIfUppercase+"|| lowercase: "+passwordType.passwordIfLowercase+"|| numeric: "+passwordType.passwordIfNumeric+"|| special characters: "+passwordType.passwordIfSpecialCharacters);
     var password = generatePassword(passwordType);
     var passwordText=document.querySelector("#password");
